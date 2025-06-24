40b6e5be33c25bd6cfeeef4f67688bc3b74d9ac4
Andreas Vege <andyvege@gmail.com>
2025-06-24 14:03:40 +0200

    Jobbet på front-end og fikk endelig implementert tailwind

--- ikko/src/app/(frontend)/layout.tsx
+++ ikko/src/app/(frontend)/layout.tsx
@@ -1,6 +1,11 @@
 import React from 'react'
 import './styles.css'
+import { Matemasie } from 'next/font/google'
 
+const matem = Matemasie({
+  subsets: ['latin'],
+  weight: ['400'], // eller flere om tilgjengelig
+})
 export const metadata = {
   description: 'A blank template using Payload in a Next.js app.',
   title: 'Payload Blank Template',
@@ -10,8 +15,16 @@ export default async function RootLayout(props: { children: React.ReactNode }) {
   const { children } = props
 
   return (
-    <html lang="en">
+    <html lang="no">
       <body>
+        <nav className="bg-white shadow-2xs">
+          <div className="flex space-x-4 p-5 text-xl text-black">
+              <a href="#" className="text-blue-700 hover:text-black">ikko</a>
+              <a href="#" className="hover:text-blue-700">hjem</a>
+              <a href="#" className="hover:text-blue-700">oppskrifter</a>
+              <a href="#" className="hover:text-blue-700">info</a>
+          </div>
+        </nav>
         <main>{children}</main>
       </body>
     </html>
